600022b984ad1a884d5ae6d27a1289ad0da5cedf
alinnert <alinnert@live.de>
2016-12-12 11:51:15 +0100

    Rebuild with commander library.

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 #!/usr/bin/env node
-
+const commander = require('commander');
 const fs = require('fs');
 const chalk = require('chalk');
 const md = require('markdown-it')({
-    breaks: true,
-    linkify: true,
-    typographer: true,
-    quotes: '„“‚‘'
+  breaks: true,
+  linkify: true,
+  typographer: true,
+  quotes: '„“‚‘'
 });
 
-if (process.argv.length <= 2) {
-    console.log(`Usage: ${chalk.blue('mdown filename [cssFile]')}`);
-    process.exit(0);
-}
+commander.version('2.0.0');
+commander.usage('[options] <files...>');
+commander.option('-s, --stylesheet [css-path]', 'Define path to the css file that should be used.');
+commander.parse(process.argv);
 
-const [nodePath, scriptPath, mdFile, cssFile] = process.argv;
-
+const files = commander.args;
 const encoding = 'UTF-8';
-let mdFileContent, cssFileContent;
+let mdFileContents, cssFileContent;
 
-try { mdFileContent = fs.readFileSync(mdFile, {encoding}); }
-catch (e) {
-    console.error(e);
-    process.exit(0);
+console.log('');
+
+try {
+  mdFileContents = files.map(file => fs.readFileSync(file, {encoding}));
+  cssFileContent = commander.stylesheet ? fs.readFileSync(commander.stylesheet, {encoding}) : '';
+}
+catch (error) {
+  switch (error.code) {
+    case 'ENOENT': {
+      console.log(`${chalk.red('  Error:\n  The file')} ${error.path} ${chalk.red(' does not exist.')}`);
+      process.exit(1);
+      break;
+    }
+    default: {
+      console.error(`${chalk.red('  Unknown error:')}\n${error.Error}`);
+    }
+  }
 }
 
-try { cssFileContent = cssFile ? fs.readFileSync(cssFile, {encoding}) : ''; }
-catch (e) { cssFileContent = ''; }
+mdFileContents.forEach((content, index) => {
+  const renderedHtml = md.render(content);
+  const mdFile = files[index];
+  const html = `<html><head><meta charset="utf-8"><title>${mdFile}</title><style>${cssFileContent}</style></head><body><div id="wrapper">${renderedHtml}</div></body></html>`;
+  const targetFileName = mdFile.replace(/\.md$/, '.html');
 
-const renderedHtml = md.render(mdFileContent);
+  console.log(`${chalk.blue('  Output file:')} ${targetFileName}`);
 
-const html = `
-<html>
-<head>
-<meta charset="utf-8">
-<title>${mdFile}</title>
-<style>${cssFileContent}</style>
-</head>
-<body>
-    <div id="wrapper">
-        ${renderedHtml}
-    </div>
-</body>
-</html>
-`;
-
-const outputFile = mdFile.replace(/\.md$/, '.html');
-
-console.log(chalk.green('DONE!'));
-console.log(`output file: ${chalk.blue(outputFile)}`);
-
-try {fs.writeFileSync(outputFile, html, {encoding});}
-catch (e) { console.error(e); }
+  try {
+    fs.writeFileSync(targetFileName, html, {encoding});
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
--- readme.md
+++ readme.md
@@ -1,25 +1,49 @@
 # Markdown CLI
-
-A CLI to convert md to html optionally using a stylesheet file.
+A CLI to convert md to html, optionally using a stylesheet file.
 
 ## Solved problem
-
 You have a .md file and quickly want to create a .html file? Maybe also using your own CSS? Here you go.
 
 ## Install
-
-~~~
-npm i -g mdcli
+~~~bash
+$ npm i -g mdcli
 ~~~
 
 ## Usage
+~~~bash
+$ mdown --help
 
-~~~
-mdown input-md-file [ stylesheet-file ]
+  Usage: index [options] <files...>
+
+  Options:
+
+    -h, --help                   output usage information
+    -V, --version                output the version number
+    -s, --stylesheet [css-path]  Define path to the css file that should be used.
 ~~~
 
-**Example:**
+### No stylesheets
+~~~bash
+$ mdown readme.md
+~~~
 
+### Using a custom *.css file
+~~~bash
+$ mdown --stylesheet readme.css readme.md
+$ mdown -s readme.css readme.md
 ~~~
-mdown inputfile.md style.css
+
+### Multiple files
+~~~bash
+$ mdown readme-1.md readme-2.md readme-3.md
 ~~~
+
+This will generate the files `readme-1.html`, `readme-2.html` and `readme-3.html`.
+
+## Changelog
+### 2.0.0
+- **[NEW]** Rebuild with [`commander`](https://github.com/tj/commander.js).
+- **[NEW]** Added support for processing multiple files at once.
+
+### 1.0.0
+Initial release
